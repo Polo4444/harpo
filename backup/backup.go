10459871e958ef392821a65b7c9607a37d09480e
Polo <sespolo@gmail.com>
2024-06-25 19:59:08 +0100

    feat: engine ready

--- backup/backup.go
+++ backup/backup.go
@@ -2,11 +2,13 @@ package backup
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/Polo44444/harpo/alerting"
 	"github.com/Polo44444/harpo/config"
 	"github.com/Polo44444/harpo/storing"
+	"github.com/go-co-op/gocron/v2"
 )
 
 type CtxString string
@@ -14,9 +16,10 @@ type CtxString string
 type Engine struct {
 	ctx       context.Context
 	cancel    context.CancelFunc
+	sch       gocron.Scheduler
 	folders   []config.Folder
-	storages  map[string]storing.Provider
-	notifiers map[string]alerting.Provider
+	storages  map[string]storing.Provider  // TODO: Should be changed to sync.Map
+	notifiers map[string]alerting.Provider // TODO: Should be changed to sync.Map
 }
 
 // Processes contexts keys
@@ -24,6 +27,12 @@ const (
 	// Holds the archive *os.File pointer coming from archive process.
 	// Please make sure you close the file after you are done with it.
 	ArchiveCtxKey CtxString = "archive"
+)
+
+const (
+	harpoTag             = "harpo"
+	harpoBackupTag       = "harpo:backup"
+	harpoBackupFolderTag = "harpo:backup:%s"
 )
 
 var (
@@ -44,14 +53,86 @@ func NewEngine(folders []config.Folder, storages map[string]storing.Provider, no
 	}
 }
 
-// Run starts the backup process
-func (e *Engine) Run() error {
+// Build Jobs build a list of jobs for the given folders
+func (e *Engine) BuildJobs() error {
+
+	var err error
+
+	// Create scheduler
+	e.sch, err = gocron.NewScheduler()
+	if err != nil {
+		return fmt.Errorf("unable to create scheduler: %w", err)
+	}
+
+	// Loop trough folders to create cron jobs
+	for _, folder := range e.folders {
+
+		// Build folder storages
+		folderStorages := map[string]storing.Provider{}
+		for _, storageName := range folder.Storages {
+			if storage, ok := e.storages[storageName]; ok {
+				folderStorages[storageName] = storage
+			}
+		}
+
+		// Build folder notifiers
+		folderNotifiers := map[string]alerting.Provider{}
+		for _, notifierName := range folder.Notifiers {
+			if notifier, ok := e.notifiers[notifierName]; ok {
+				folderNotifiers[notifierName] = notifier
+			}
+		}
+
+		// Create cron job
+		_, err := e.sch.NewJob(
+			gocron.CronJob(folder.Schedule, false),
+			gocron.NewTask(func(folder config.Folder, stors map[string]storing.Provider, notifs map[string]alerting.Provider) {
+				e.ProcessFolder(folder, stors, notifs)
+			},
+				folder,
+				folderStorages,
+				folderNotifiers,
+			),
+			gocron.WithTags(harpoTag, harpoBackupTag, fmt.Sprintf(harpoBackupFolderTag, folder.Name)),
+		)
+		if err != nil {
+			e.RemoveJobs()
+			return fmt.Errorf("unable to create cron job of folder %s: %w", folder.Name, err)
+		}
+	}
 
 	return nil
 }
 
+// start starts the backup process in background and returns immediately.
+func (e *Engine) Start() {
+	e.sch.Start()
+}
+
+// Run starts the backup process and blocks until the process is finished.
+func (e *Engine) Run() {
+
+	e.Start()
+	<-e.ctx.Done() // Wait until the context is done
+}
+
+// RemoveJobs removes all scheduled jobs
+func (e *Engine) RemoveJobs() {
+	e.sch.RemoveByTags(harpoTag)
+}
+
+// RemoveJob removes the scheduled job of the given folder
+func (e *Engine) RemoveJob(folderName string) {
+	e.sch.RemoveByTags(fmt.Sprintf(harpoBackupFolderTag, folderName))
+}
+
+// StopJobs stops all scheduled jobs
+func (e *Engine) StopJobs() error {
+	return e.sch.StopJobs()
+}
+
 // ProcessFolder processes the given folder. It archives and uploads the folder to the storages.
-func (e *Engine) ProcessFolder() error {
+func (e *Engine) ProcessFolder(folder config.Folder, storages map[string]storing.Provider, notifiers map[string]alerting.Provider) error {
 
 	// Setup chain
 	chain := NewArchiver()
@@ -62,12 +143,14 @@ func (e *Engine) ProcessFolder() error {
 	// Execute chain
 	ctx, cancel := context.WithTimeout(e.ctx, ProcessTimeout)
 	defer cancel()
-	chain.process(ctx, e.folders[0], e.storages, e.notifiers)
+	chain.process(ctx, folder, storages, notifiers)
 
 	return nil
 }
 
-// Stop stops the backup process and closes the providers. Make sure to create new providers if you want to start the backup process again.
+// Stop cancels the engine context, shutdowns the engine and returns immediately.
 func (e *Engine) Stop() error {
-	return nil
+
+	e.cancel()
+	return e.sch.Shutdown()
 }
--- go.mod
+++ go.mod
@@ -35,17 +35,21 @@ require (
 	github.com/bodgit/windows v1.0.0 // indirect
 	github.com/connesc/cipherio v0.2.1 // indirect
 	github.com/dsnet/compress v0.0.1 // indirect
+	github.com/go-co-op/gocron/v2 v2.7.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/klauspost/compress v1.17.7 // indirect
 	github.com/klauspost/pgzip v1.2.5 // indirect
 	github.com/nwaples/rardecode/v2 v2.0.0-beta.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/therootcompany/xz v1.0.1 // indirect
 	github.com/ulikunitz/xz v0.5.10 // indirect
 	go4.org v0.0.0-20200411211856-f5505b9728dd // indirect
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
